457cbdd01c80987fd78a19d87a0c725e4154d96b
ZTZach <ZTZach@85f1a612-6cfb-11de-83ec-e78895e4d208>
2009-07-27 04:59:34 +0000

    Initial Commit

---
+++ trunk/ccb-dynamic-calendar-embed/src/com/relevantwalk/churchcalembed/client/ChurchCalendarEmbed.java
@@ -0,0 +1,10 @@
+package com.relevantwalk.churchcalembed.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class ChurchCalendarEmbed implements EntryPoint {
+	
+	public void onModuleLoad() {
+		
+	}
+}
